ed532d387505da44cd7eddf25bf41262eceae89e
mohammad ziadeh <mohammedziadeh6@gmail.com>
2025-02-17 21:46:28 +0300

    not today commit

--- tasks/day5.php
+++ tasks/day5.php
@@ -52,16 +52,84 @@
 // };
 // echo '</br>';
 //7-------------------------------------
-
+// $nums = [1, 19, 9, 4];
+// $maxNumber = max($nums);
+// echo $maxNumber;
 //8-------------------------------------
-$electric_bill = 166;
-if ($electric_bill <= 50) {
-    $unit = 2.50;
-} elseif ($electric_bill <= 150) {
-    $unit = 5.00;
-} elseif ($electric_bill <= 250) {
-    $unit = 6.20;
-} else {
-    $unit = 7.50;
-}
-echo $electric_bill * $unit;
+// $units  = 140;
+// $price = 0;
+// if ($units <= 50) {
+//     $price = 2.5 * $units;
+// } elseif ($units > 50 && $units <= 150) {
+//     $price = 5 * $units;
+// } elseif ($units > 150 && $units <= 250) {
+//     $price = 6.2 * $units;
+// } else {
+//     $price = 7.5 * $units;
+// }
+// echo $price;
+//9-------------------------------------
+// $num1 = 4;
+// $num2 = 8;
+// $result = 0;
+// $operation = '-';
+// switch ($operation) {
+//     case '+':
+//         $result = $num1 + $num2;
+//         break;
+
+//     case '-':
+//         $result = $num1 - $num2;
+//         break;
+
+//     case '*':
+//         $result = $num1 * $num2;
+//         break;
+
+//     case '/':
+//         $result = $num1 / $num2;
+//         break;
+
+//     default:
+//         $result = "not valid operator";
+// }
+// echo $result;
+
+//10-------------------------------------
+// $age = 19;
+// if ($age >= 18) {
+//     echo 'eligible to vote';
+// } else {
+//     echo 'not eligible to vote';
+// }
+
+//11-------------------------------------
+// $number = -10;
+// if ($number < 0) {
+//     echo 'negative';
+// } elseif ($number > 0) {
+//     echo 'positive';
+// } else {
+//     echo "zero";
+// }
+
+//12-------------------------------------
+// $marks = [90, 86, 95, 63, 80, 74, 79, 82, 70];
+//         $sum = 0;
+//         $avg = 0;
+//         $count = count($marks);
+//         for ($i = 0; $i < $count; $i++) {
+//             $sum += $marks[$i];
+//         }
+//         $avg = $sum / $count;
+//         if ($avg < 60) {
+//             echo 'F';
+//         } elseif ($avg < 70) {
+//             echo 'D';
+//         } elseif ($avg < 80) {
+//             echo 'C';
+//         } elseif ($avg < 90) {
+//             echo 'B';
+//         } elseif ($avg < 100) {
+//             echo 'A';
+//         }
